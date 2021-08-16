3700e6beee7670efa0e3169f124e42433356ec9e
Javier Suarez <javiersuarezdominguez@gmail.com>
2021-08-16 17:20:47 -0300

    Creating Card from React Boostrap to visualize the weather information

--- src/Components/Board/index.js
+++ src/Components/Board/index.js
@@ -1,7 +1,27 @@
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
+
 function Board () {
     return (
         <div>
-            <h2>Buenos Aires</h2>
+
+            <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="" />
+            <Card.Body>
+                <Card.Title>Buenos Aires</Card.Title>
+                <Card.Text>clear sky</Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+                <ListGroupItem>°C</ListGroupItem>
+                <ListGroupItem>Max: °C</ListGroupItem>
+                <ListGroupItem>Min: °C</ListGroupItem>
+            </ListGroup>
+            <Card.Body>
+                <Card.Link href="#">Card Link</Card.Link>
+                <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+            </Card>
             
         </div>
     )
